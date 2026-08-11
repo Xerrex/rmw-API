c92f5df039eb2e1ad708c6109c4eaafd6e496b39
Alex Kagai <alexkagai@outlook.com>
2026-08-11 12:14:41 +0300

    Remove not required details for user details

--- auth/router.py
+++ auth/router.py
@@ -269,4 +269,8 @@ def get_user_details(current_user: UserSchema = Depends(get_current_user)):
         current_user (UserSchema, optional): Details of the currently logged in user. Defaults to Depends(get_current_user).
     """
 
-    return current_user;
+    return {
+        "first_name": current_user.first_name,
+        "last_name": current_user.last_name,
+        "email": current_user.email
+    };
